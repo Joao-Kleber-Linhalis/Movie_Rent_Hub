4550c716e780cd20cdbc39712a48cbce798d05b0
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-09-17 11:30:25 -0300

    Diretor CRUD

--- BackEndLocadora/src/main/java/com/jk/BackEndLocadora/controller/DiretorController.java
+++ BackEndLocadora/src/main/java/com/jk/BackEndLocadora/controller/DiretorController.java
@@ -1,10 +1,16 @@
 package com.jk.BackEndLocadora.controller;
 
+import com.jk.BackEndLocadora.domain.dto.DiretorDTO;
 import com.jk.BackEndLocadora.service.DiretorService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -13,4 +19,37 @@ public class DiretorController {
 
     @Autowired
     private DiretorService diretorService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DiretorDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(diretorService.findById(id));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<DiretorDTO>> findAllByStatus(@RequestParam("status") String status) {
+        if ("ativos".equals(status)) {
+            return ResponseEntity.ok().body(diretorService.findAllByStatus(true));
+        } else if ("inativos".equals(status)) {
+            return ResponseEntity.ok().body(diretorService.findAllByStatus(false));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<DiretorDTO> create(@Valid @RequestBody DiretorDTO diretorDTO) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(diretorService.create(diretorDTO).getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<DiretorDTO> update(@PathVariable Long id, @Valid @RequestBody DiretorDTO diretorDTO) {
+        return ResponseEntity.ok().body(diretorService.update(id, diretorDTO));
+    }
+
+    @PutMapping(value = "/desabilitar/{id}")
+    public ResponseEntity<DiretorDTO> desabilitar(@PathVariable Long id) {
+        return ResponseEntity.ok().body(diretorService.disable(id));
+    }
+
 }
--- BackEndLocadora/src/main/java/com/jk/BackEndLocadora/service/DiretorService.java
+++ BackEndLocadora/src/main/java/com/jk/BackEndLocadora/service/DiretorService.java
@@ -1,11 +1,88 @@
 package com.jk.BackEndLocadora.service;
 
+import com.jk.BackEndLocadora.domain.Diretor;
+import com.jk.BackEndLocadora.domain.dto.AtorDTO;
+import com.jk.BackEndLocadora.domain.dto.DiretorDTO;
+import com.jk.BackEndLocadora.exceptions.ObjectNotFoundException;
+import com.jk.BackEndLocadora.repository.DiretorRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class DiretorService {
+
+    @Autowired
+    private DiretorRepository diretorRepository;
+
+    ModelMapper modelMapper = new ModelMapper();
+
+    public DiretorDTO findById(Long id) {
+        Optional<Diretor> diretor = diretorRepository.findByIdAndAtivo(id, true);
+        return converterOptionalDiretorParaDTO(diretor).orElseThrow(() -> new ObjectNotFoundException(findByIdDisable(id) + " Id:" + id));
+    }
+
+    public List<DiretorDTO> findAllByStatus(Boolean status) {
+        List<Diretor> diretorList = diretorRepository.findByStatus(status);
+        return diretorList.stream().map(diretor -> modelMapper.map(diretor, DiretorDTO.class)).collect(Collectors.toList());
+    }
+
+    public DiretorDTO create(DiretorDTO diretorDTO) {
+        diretorDTO.setId(null);
+        if (diretorRepository.findByNome(diretorDTO.getNome()).isPresent()) {
+            throw new IllegalArgumentException("Nome de Diretor já cadastrado");
+        }
+        return modelMapper.map(diretorRepository.save(modelMapper.map(diretorDTO, Diretor.class)), DiretorDTO.class);
+    }
+
+    public DiretorDTO update(Long id, DiretorDTO diretorDTO) {
+        diretorDTO.setId(id);
+        DiretorDTO diretorDTO1 = findById(id);
+        if (!diretorDTO.getAtivo() && diretorDTO1.getAtivo()) {
+            return disable(id);
+        } else {
+            return modelMapper.map(diretorRepository.save(modelMapper.map(diretorDTO, Diretor.class)), DiretorDTO.class);
+        }
+
+
+    }
+
+    public DiretorDTO disable(Long id) {
+        Diretor diretor = modelMapper.map(findById(id), Diretor.class);
+        List<String> titulos = diretorRepository.findTitulosByDiretorId(id);
+        if (!titulos.isEmpty()) {
+            String nomesTitulos = String.join(", ", titulos);
+            throw new DataIntegrityViolationException("Diretor possui " + titulos.size() + " filmes associados a ele. Nomes:  " + nomesTitulos);
+        }
+        diretor.setAtivo(false);
+        return modelMapper.map(diretorRepository.save(diretor), DiretorDTO.class);
+    }
+
+
+    private Optional<DiretorDTO> converterOptionalDiretorParaDTO(Optional<Diretor> optionalDiretor) {
+        return optionalDiretor.map(diretor -> modelMapper.map(diretor, DiretorDTO.class));
+    }
+
+    private Optional<Diretor> converterOptionalDTOparaDiretor(Optional<DiretorDTO> optionalDiretorDTO) {
+        return optionalDiretorDTO.map(diretorDTO -> modelMapper.map(diretorDTO, Diretor.class));
+    }
+
+    private String findByIdDisable(Long id) {
+        Optional<Diretor> diretor = diretorRepository.findByIdAndAtivo(id, false);
+        Optional<DiretorDTO> diretorDTO = converterOptionalDiretorParaDTO(diretor);
+        if (diretorDTO.isPresent()) {
+            return "Objeto atualmente desativado";
+        } else {
+            return "Objeto não encontrado";
+        }
+    }
 }
