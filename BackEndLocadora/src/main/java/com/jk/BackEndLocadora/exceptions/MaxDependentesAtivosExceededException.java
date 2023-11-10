c4aba8b32e16a5108d16161332f417c549860a2b
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-11-10 15:00:00 -0300

    Cliente e Dependentes CRUD

--- BackEndLocadora/src/main/java/com/jk/BackEndLocadora/exceptions/MaxDependentesAtivosExceededException.java
+++ BackEndLocadora/src/main/java/com/jk/BackEndLocadora/exceptions/MaxDependentesAtivosExceededException.java
@@ -1,2 +1,7 @@
-package com.jk.BackEndLocadora.exceptions;public class MaxDependentesAtivosExceededException {
+package com.jk.BackEndLocadora.exceptions;
+
+public class MaxDependentesAtivosExceededException extends RuntimeException {
+    public MaxDependentesAtivosExceededException(String message) {
+        super(message);
+    }
 }
